486a53bdeb75ad3f7cec128c4002e91f7c4461c1
henritresor <shimwamanahenritresor@gmail.com>
2025-05-18 15:04:54 +0200

    implemented PUT and DELETE

--- frontend/src/helpers/api.ts
+++ frontend/src/helpers/api.ts
@@ -1,16 +1,15 @@
+import { generateHeaders } from "../lib/utils"
+
 const BASE_URL = import.meta.env.VITE_API_BASE_URL
+
 
 
 const api = {
     POST: async <T = any>(uri: string, body: Record<string, any>): Promise<T> => {
         const token = localStorage.getItem('token')
-        const headers = {
-            'Content-Type': "application/json",
-            'Authorization': `Bearer ${token}`
-        }
         const response = await fetch(`${BASE_URL}${uri}`, {
-            method:'POST',
-            headers,
+            method: 'POST',
+            headers: generateHeaders(token as string),
             body: JSON.stringify(body)
         })
 
@@ -19,21 +18,35 @@ const api = {
     },
     GET: async (uri: string) => {
         const token = localStorage.getItem('token')
-        const headers = {
-            'Content-Type': "application/json",
-            'Authorization': `Bearer ${token}`
-        }
-        console.log(token)
         const response = await fetch(`${BASE_URL}${uri}`, {
-            method:'GET',
-            headers
+            method: 'GET',
+            headers: generateHeaders(token as string)
         })
 
-        const data = response.json()
+        const data = await response.json()
         return data
     },
-    PUT: async () => { },
-    DELETE: async () => { }
+    PUT: async (uri: string, body: Record<string, any>) => {
+        const token = localStorage.getItem('token')
+        const response = await fetch(`${BASE_URL}${uri}`, {
+            headers: generateHeaders(token as string),
+            method: "PUT",
+            body: JSON.stringify(body)
+        })
+
+        const data = await response.json()
+        return data
+    },
+    DELETE: async (uri: string) => {
+        const token = localStorage.getItem('token')
+        const response = await fetch(`${BASE_URL}${uri}`, {
+            headers: generateHeaders(token as string),
+            method: "DELETE"
+        })
+
+        const data = await response.json()
+        return data
+    }
 }
 
 
